3eb27cd8d927e06432c4bc7660915ad99cd7a39f
SEEDOFT <koyyotrabothffc001@gmail.com>
2026-06-01 13:09:37 +0700

    feat: seed profiles, wallets, and payment methods for default accounts

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\PaymentMethodStatus;
+use App\Models\PaymentMethodType;
 use App\Models\User;
 use App\Models\UserStatus;
 use App\Models\UserType;
@@ -47,13 +49,54 @@ class UserSeeder extends Seeder
         ];
 
         foreach ($users as $userData) {
-            User::firstOrCreate(
+            $user = User::firstOrCreate(
                 [
                     'phone_number' => $userData['phone_number'],
                     'user_type_id' => $userData['user_type_id']
                 ],
                 $userData
             );
+
+            if ($user->wasRecentlyCreated) {
+                if ($user->user_type_id === UserType::ADMIN_ID) {
+                    $user->adminProfile()->create([
+                        'locale' => 'km',
+                        'theme' => 'system',
+                        'super_admin' => true,
+                    ]);
+                    $user->ensureDefaultWallets();
+                } elseif ($user->user_type_id === UserType::VENDOR_ID) {
+                    $user->vendorProfile()->create([
+                        'business_name' => 'Test Vendor',
+                        'contact_phone' => '+85581968185',
+                        'village' => 'Test Village',
+                        'commune' => 'Test Commune',
+                        'district' => 'Test District',
+                        'province' => 'Phnom Penh',
+                        'id_card_front' => 'default.png',
+                        'id_card_back' => 'default.png',
+                        'store_front_image' => 'default.png',
+                    ]);
+        
+                    $user->vendorFinancialDetails()->create([
+                        'payment_method_type_id' => PaymentMethodType::ABA_ID ?? 1,
+                        'payment_method_status_id' => PaymentMethodStatus::ACTIVE_ID,
+                        'bank_name' => 'ABA Bank',
+                        'account_name' => 'Test Vendor',
+                        'account_number' => '123456789',
+                        'qr_code' => 'default.png',
+                    ]);
+        
+                    $user->ensureDefaultWallets();
+                } elseif ($user->user_type_id === UserType::CONSUMER_ID) {
+                    $user->userProfile()->create([
+                        'locale' => 'km',
+                        'theme' => 'system',
+                    ]);
+                    $user->ensureDefaultWallets();
+                    $user->ensureDefaultPaymentMethod();
+                }
+            }
         }
     }
 }
